f45a8357396da82d6c69cf813533e93f76867629
Abhinraj-ss <abhinrajss18@gmail.com>
2022-07-11 20:13:11 +0530

    added procfile

---
+++ App.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import psycopg2
+Host = "ec2-52-20-166-21.compute-1.amazonaws.com"
+Database = "d6re5m4gmvtalp"
+User = "ybeylittvmuyzj"
+Port= 5432
+Password = "75d7775c33b597e7a3269903dfbfc8f1a79018f6c252581d9ad3768991cffb1d"
+
+app = Flask(__name__)
+
+conn = psycopg2.connect(dbname = Database, user = User ,password = Password, host = Host)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+conn.close()
